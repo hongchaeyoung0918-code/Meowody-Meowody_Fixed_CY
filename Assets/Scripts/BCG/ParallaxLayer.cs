69bdb415bb5bf84d060e5e0080c12fe3cb7306d5
hlj1085 <113406145+hlj1085@users.noreply.github.com>
2026-05-25 00:37:24 +0900

    BCG Refactoring

--- Assets/Scripts/BCG/ParallaxLayer.cs
+++ Assets/Scripts/BCG/ParallaxLayer.cs
@@ -3,10 +3,11 @@ using UnityEngine;
 public class ParallaxLayer : MonoBehaviour
 {
     public Transform cameraTransform;
-    [Tooltip("1이면 카메라와 완벽 동기화, 0이면 고정(가장 빠르게 지나감)")]
     [Range(0f, 1f)] public float scrollFactorX;
 
-    private float length;
+    public float customLength = 0f;
+
+    private float singleImageLength;
     private float startPosX;
     private Camera mainCam;
 
@@ -17,47 +18,35 @@ public class ParallaxLayer : MonoBehaviour
 
         startPosX = transform.position.x;
 
-        var spriteRenderer = GetComponent<SpriteRenderer>();
-        if (spriteRenderer != null)
+        // 인스펙터에 기획자가 너비를 적었다면 그 값을 최우선으로 사용합니다. (가장 안전)
+        if (customLength > 0f)
         {
-            length = spriteRenderer.bounds.size.x;
+            singleImageLength = customLength;
         }
         else
         {
-            // 자식 오브젝트들에 스프라이트가 여러 장 붙어있는 경우 전체 너비 계산
-            var renderers = GetComponentsInChildren<SpriteRenderer>();
-            float minX = float.MaxValue;
-            float maxX = float.MinValue;
-            foreach (var r in renderers)
-            {
-                minX = Mathf.Min(minX, r.bounds.min.x);
-                maxX = Mathf.Max(maxX, r.bounds.max.x);
-            }
-            length = maxX - minX;
+            // 적지 않았다면 기존 자동 계산 로직 실행
+            var spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+            if (spriteRenderer != null) singleImageLength = spriteRenderer.bounds.size.x;
         }
+
+        Debug.Log($"{gameObject.name}의 루프 너비 설정값: {singleImageLength}");
     }
 
     void LateUpdate()
     {
-        // 1. 패럴렉스 공식에 따른 이동
         float distance = cameraTransform.position.x * scrollFactorX;
         transform.position = new Vector3(startPosX + distance, transform.position.y, transform.position.z);
 
-        // 2. 가상의 '카메라 상대 위치' 계산
         float temp = cameraTransform.position.x * (1 - scrollFactorX);
 
-        // 3. 끊김 방지를 위한 버퍼 추가 (카메라 가로 시야 절반 크기)
-        float cameraHalfWidth = mainCam.orthographicSize * mainCam.aspect;
-
-        // 카메라가 현재 배경 영역의 오른쪽 경계를 넘어서려고 하면, 배경을 오른쪽으로 한 칸 이동
-        if (temp > startPosX + length - cameraHalfWidth)
+        if (temp > startPosX + singleImageLength)
         {
-            startPosX += length;
+            startPosX += singleImageLength;
         }
-        // 반대의 경우 왼쪽으로 이동
-        else if (temp < startPosX - length + cameraHalfWidth)
+        else if (temp < startPosX - singleImageLength)
         {
-            startPosX -= length;
+            startPosX -= singleImageLength;
         }
     }
 }
